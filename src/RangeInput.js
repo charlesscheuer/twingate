b18f006f0a61257ddefe38c91d24771cd93812bc
charlesscheuer <cscheuer@wustl.edu>
2021-09-20 19:04:32 -0700

    clearing console issues

--- src/RangeInput.js
+++ src/RangeInput.js
@@ -1,5 +1,4 @@
 import React, {
-  useCallback,
   useEffect,
   //   useMemo,
   useRef,
@@ -16,49 +15,21 @@ const RangeInput = (props) => {
   const step = 1;
   const logScale = new LogScale(0, 10000);
 
-  // const changeInputProgressPercentStyle = useCallback(() => {
-
-  //   // console.log(
-  //   //   "ran change %%%",
-  //   //   logScale.logarithmicToLinear(props.users) * 100
-  //   // );
-  //   // inputRef.current.style.setProperty(
-  //   //   "--webkitProgressPercent",
-  //   //   `${logScale.logarithmicToLinear(props.users) * 100}%`
-  //   // );
-  // }, [props.users]);
-
   useEffect(() => {
-    // changeInputProgressPercentStyle();
     const inputElement = inputRef.current;
 
     const handleUpAndLeave = () => setIsChanging(false);
     const handleDown = () => setIsChanging(true);
 
-    // inputElement.addEventListener("mousemove", changeInputProgressPercentStyle);
     inputElement.addEventListener("mousedown", handleDown);
-    // inputElement.addEventListener("dragstart", handleDown);
-    // inputElement.addEventListener("dragend", handleUpAndLeave);
     inputElement.addEventListener("mouseup", handleUpAndLeave);
     inputElement.addEventListener("mouseleave", handleUpAndLeave);
     return () => {
-      // inputElement.removeEventListener(
-      //   "mousemove"
-      //   // changeInputProgressPercentStyle
-      // );
       inputElement.removeEventListener("mousedown", handleDown);
       inputElement.removeEventListener("mouseup", handleUpAndLeave);
       inputElement.removeEventListener("mouseleave", handleUpAndLeave);
     };
   }, [isChanging]);
-
-  useEffect(() => {
-    if (!inputRef?.current) return;
-    // setIsLoaded(true);
-    // changeInputProgressPercentStyle();
-  }, [inputRef]);
-
-  console.log(logScale.logarithmicToLinear(props.users) * max, "is the valuee");
 
   return (
     <input
